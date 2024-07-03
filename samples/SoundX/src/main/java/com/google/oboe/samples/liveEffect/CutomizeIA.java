1ee70c74237072430a4a181dce362888dfd92663
Simon Cornaz <simon.cornaz@dont-nod.com>
2024-07-03 16:09:44 +0200

    WIP: layout

---
+++ samples/SoundX/src/main/java/com/google/oboe/samples/liveEffect/CutomizeIA.java
@@ -0,0 +1,42 @@
+package com.google.oboe.samples.liveEffect;
+
+import android.app.AlertDialog;
+import android.app.Dialog;
+import android.content.DialogInterface;
+import android.os.Bundle;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.DialogFragment;
+
+public class CutomizeIA extends DialogFragment {
+    public boolean[] CheckItems = null;
+    public CharSequence[] Presets = null;
+
+    public void setPresets(String[] P)
+    {
+        Presets = P;
+        if (CheckItems == null)
+        {
+            CheckItems = new boolean[Presets.length];
+        }
+    }
+
+    @NonNull
+    @Override
+    public Dialog onCreateDialog(Bundle savedInstanceState) {
+        AlertDialog.Builder builder = new AlertDialog.Builder(getActivity());
+
+        builder
+                .setTitle("Vibration Preset")
+                .setPositiveButton("OK", (dialog, which) -> {
+                })
+                .setMultiChoiceItems(Presets, CheckItems,
+                        new DialogInterface.OnMultiChoiceClickListener() {
+                            @Override
+                            public void onClick(DialogInterface dialog, int which,
+                                                boolean isChecked) {
+                            }
+                        });
+        return builder.create();
+    }
+}
